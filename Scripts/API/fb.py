10564c64bd7a847285a28aa246b90cb960b0a210
“Paurakh977” <pandeypaurakhraj@gmail.com>
2024-07-08 18:28:28 +0545

    FB api cancel for now

--- Scripts/API/fb.py
+++ Scripts/API/fb.py
@@ -1,4 +1,4 @@
-import sys
+import sys,signupp
 import webbrowser
 import requests
 from flask import Flask, request
@@ -6,7 +6,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout,
 from PyQt5.QtCore import QThread, pyqtSignal, QObject
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
-# Flask application setup
+
 app = Flask(__name__)
 app.secret_key = '6a0cc1c6ba2bf2304d9f0bc36d825456'  # Your secret key here
 
@@ -16,10 +16,6 @@ client_secret = '6a0cc1c6ba2bf2304d9f0bc36d825456'
 redirect_uri = 'http://localhost:5000/callback'
 scope = 'email'
 
-# Custom signal class
-class SignalEmitter(QObject):
-    window_closed = pyqtSignal()
-
 # PyQt5 application window
 class LoginWindow(QMainWindow):
     def __init__(self):
@@ -27,22 +23,30 @@ class LoginWindow(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(600,200,600,800)
+        self.setGeometry(600, 200, 600, 800)
         self.setWindowTitle("SAMADHAN")
-        loadUi(r"C:\Users\pande\OneDrive\Desktop\PyQt\Samadhan-App\UI\Login.ui",self)
+        loadUi(r"C:\Users\pande\OneDrive\Desktop\PyQt\Samadhan-App\UI\Login.ui", self)
 
-        self.go_to_signup=self.findChild(QtWidgets.QPushButton,"signup_btn")
-        self.login_btn=self.findChild(QtWidgets.QAbstractButton,"login_btn")
+        self.go_to_signup = self.findChild(QtWidgets.QPushButton, "signup_btn")
+        self.login_btn = self.findChild(QtWidgets.QAbstractButton, "login_btn")
         
-        self.fb_login_btn=self.findChild(QPushButton,"fb_btn")
+        self.fb_login_btn = self.findChild(QPushButton, "fb_btn")
         self.fb_login_btn.clicked.connect(self.login_with_facebook)
-       
+        
+
+        self.go_to_signup.clicked.connect(self.get_signup_window)
+    def get_signup_window(self):
+        signup_window = signupp.SignupWindow()  # Create an instance of SignupWindow
+        signup_window.show()  # Show the SignupWindow
+        self.close() 
+        
     def login_with_facebook(self):
         auth_url = f'https://www.facebook.com/v20.0/dialog/oauth?client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}'
         webbrowser.open(auth_url)
 
     def closeEvent(self, event):
-        self.signal_emitter.window_closed.emit()  # Emit the custom signal when window is closed
+        # Shutdown Flask server when closing the window
+        shutdown_server()
         event.accept()
 
 # Flask routes
@@ -95,12 +99,10 @@ if __name__ == '__main__':
     server_thread = QThread()
     server_thread.run = lambda: app.run(debug=False)
     server_thread.start()
-
+    
     # Start PyQt5 application
     app = QApplication(sys.argv)
-    signal_emitter = SignalEmitter()  # Create an instance of the custom signal emitter
     main_window = LoginWindow()
-    main_window.signal_emitter = signal_emitter  # Assign the signal emitter to main_window
-    signal_emitter.window_closed.connect(shutdown_server)  # Connect window_closed signal to shutdown_server
     main_window.show()
+    
     sys.exit(app.exec_())
