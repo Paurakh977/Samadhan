cb242d914a0dcd2c5f65a605a4cf07d5cf264bbc
“Paurakh977” <pandeypaurakhraj@gmail.com>
2024-07-21 22:00:20 +0545

    ciruclar progrress bar in single file

--- Scripts/circular_progress_bar.py
+++ Scripts/circular_progress_bar.py
@@ -1,8 +1,9 @@
+import sys
+import os
 from math import cos, sin, radians
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-import os
 
 class CircularProgress(QWidget):
     def __init__(self):
@@ -98,3 +99,46 @@ class CircularProgress(QWidget):
         paint.drawImage(QRect(end_x, end_y, self.end_image.width(), self.end_image.height()), self.end_image)
 
         paint.end()
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.resize(600, 600)
+
+        self.container = QFrame()
+        self.container.setStyleSheet("background-color: white")
+        self.layout = QVBoxLayout()
+
+        self.progress = CircularProgress()
+        self.progress.value = 0
+        self.progress.suffix = "%"
+        self.progress.font_size = 30
+        self.progress.width = 500
+        self.progress.height = 500
+        self.progress.progress_width = 40
+        self.progress.progress_color = QColor(22, 109, 245)
+        self.progress.progress_rounded_cap = True
+        self.progress.setMinimumSize(self.progress.width, self.progress.height)
+
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setRange(0, 100)
+        self.slider.valueChanged.connect(self.change_value)
+
+        self.layout.addWidget(self.progress, Qt.AlignCenter, Qt.AlignCenter)
+        self.layout.addWidget(self.slider, Qt.AlignCenter, Qt.AlignCenter)
+
+        self.container.setLayout(self.layout)
+        self.setCentralWidget(self.container)
+
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+
+        self.show()
+
+    def change_value(self, value):
+        self.progress.set_value(value)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec_())
