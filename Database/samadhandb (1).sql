689c3135ad4954cecd5812f5ba9edcef86e33414
Yawat-Malla <yawatmalla124@gmail.com>
2024-08-29 20:42:51 +0545

    database updated

--- Database/samadhandb (1).sql
+++ Database/samadhandb (1).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 11, 2024 at 09:00 PM
+-- Generation Time: Aug 29, 2024 at 04:54 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,24 +33,29 @@ CREATE TABLE `app_usage_info` (
   `browser` varchar(100) DEFAULT NULL,
   `SN` int(11) NOT NULL,
   `used_day` varchar(10) DEFAULT NULL,
-  `used_date` date DEFAULT NULL
+  `used_date` date DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `serial_id` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `app_usage_info`
 --
 
-INSERT INTO `app_usage_info` (`tab_name`, `used_time`, `browser`, `SN`, `used_day`, `used_date`) VALUES
-('localhost/phpmyadmin/index', 5, 'Google Chrome', 1, 'Tuesday', '2024-07-09'),
-('youtube', 15, 'Firefox', 2, 'Wednesday', '2024-07-10'),
-('instagram', 20, 'Safari', 3, 'Thursday', '2024-07-11'),
-('facebook', 45, 'Opera', 4, 'Friday', '2024-07-12'),
-('twitter', 30, 'Microsoft Edge', 5, 'Saturday', '2024-07-13'),
-('reddit', 10, 'Google Chrome', 6, 'Sunday', '2024-07-14'),
-('whatsapp', 35, 'Safari', 8, 'Tuesday', '2024-07-16'),
-('snapchat', 40, 'Opera', 9, 'Wednesday', '2024-07-17'),
-('tiktok', 50, 'Microsoft Edge', 10, 'Thursday', '2024-07-18'),
-('discord', 7, 'Microsoft Edge', 32, 'Monday', '2024-07-08');
+INSERT INTO `app_usage_info` (`tab_name`, `used_time`, `browser`, `SN`, `used_day`, `used_date`, `email`, `serial_id`) VALUES
+('localhost/phpmyadmin/index', 5, 'Google Chrome', 1, 'Tuesday', '2024-07-09', NULL, NULL),
+('youtube', 15, 'Firefox', 2, 'Wednesday', '2024-07-10', NULL, NULL),
+('instagram', 20, 'Safari', 3, 'Thursday', '2024-07-11', NULL, NULL),
+('facebook', 45, 'Opera', 4, 'Friday', '2024-07-12', NULL, NULL),
+('twitter', 30, 'Microsoft Edge', 5, 'Saturday', '2024-07-13', NULL, NULL),
+('reddit', 10, 'Google Chrome', 6, 'Sunday', '2024-07-14', NULL, NULL),
+('whatsapp', 35, 'Safari', 8, 'Tuesday', '2024-07-16', NULL, NULL),
+('snapchat', 40, 'Opera', 9, 'Wednesday', '2024-07-17', NULL, NULL),
+('tiktok', 50, 'Microsoft Edge', 10, 'Thursday', '2024-07-18', NULL, NULL),
+('discord', 7, 'Microsoft Edge', 32, 'Monday', '2024-07-08', NULL, NULL),
+('discord', 3, NULL, 33, 'Thursday', '2024-08-29', 'yareyaredazey108@gmail.com', '1bcfadd1-06b5-4086-b4e9-a5ae90da5af5'),
+('stackoverflow', 6, NULL, 34, 'Thursday', '2024-08-29', 'yareyaredazey108@gmail.com', '1bcfadd1-06b5-4086-b4e9-a5ae90da5af5'),
+('youtube', 21, NULL, 35, 'Thursday', '2024-08-29', 'yareyaredazey108@gmail.com', '1bcfadd1-06b5-4086-b4e9-a5ae90da5af5');
 
 -- --------------------------------------------------------
 
@@ -72,10 +77,11 @@ CREATE TABLE `user_info_google` (
 --
 
 INSERT INTO `user_info_google` (`ID`, `email`, `username`, `password`, `logged_in_status`, `serial_id`) VALUES
-(2, 'yareyaredazey108@gmail.com', 'Jet Blackwing', NULL, 0, NULL),
 (5, 'paurakh.pandey@sifal.deerwalk.edu.np', 'Paurakh Raj Pandey', NULL, 0, 'f8a294e2-f241-4eca-a964-c0c88c21f3c2'),
 (6, 'dollarchaeyo@gmail.com', 'Dollar Chaeyo', NULL, 0, 'f8a294e2-f241-4eca-a964-c0c88c21f3c2'),
-(7, 'pandeypaurakhraj@gmail.com', 'Paurakh Raj Pandey', NULL, 0, 'f8a294e2-f241-4eca-a964-c0c88c21f3c2');
+(7, 'pandeypaurakhraj@gmail.com', 'Paurakh Raj Pandey', NULL, 0, 'f8a294e2-f241-4eca-a964-c0c88c21f3c2'),
+(8, 'yareyaredazey108@gmail.com', 'Jet Blackwing', NULL, 1, '1bcfadd1-06b5-4086-b4e9-a5ae90da5af5'),
+(10, 'yareyaredazey108@gmail.com', 'Jet Blackwing', NULL, 1, '1bcfadd1-06b5-4086-b4e9-a5ae90da5af5');
 
 -- --------------------------------------------------------
 
@@ -133,13 +139,13 @@ ALTER TABLE `user_info_manual`
 -- AUTO_INCREMENT for table `app_usage_info`
 --
 ALTER TABLE `app_usage_info`
-  MODIFY `SN` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `SN` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `user_info_google`
 --
 ALTER TABLE `user_info_google`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `user_info_manual`
